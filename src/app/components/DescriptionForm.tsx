2950633748846acb62afb35bf88cc050a966f12e
biyamn <b1y9mn@gmail.com>
2024-10-06 23:30:22 +0900

    feat: textarea에 maxlength 추가

--- src/app/components/DescriptionForm.tsx
+++ src/app/components/DescriptionForm.tsx
@@ -11,13 +11,13 @@ const DescriptionForm = ({
 }: DescriptionFormProps) => {
   return (
     <textarea
-      className="relative -top-16 w-full px-4 z-50 bg-transparent resize-none text-lg"
+      className="relative -top-16 w-full px-4 z-50 bg-transparent resize-none text-[16px]"
       rows={2}
       placeholder="어떤 기념할 일이 있었나요?"
       disabled={!isEditing}
       value={text}
       onChange={(e) => setText(e.target.value)}
-      maxLength={28}
+      maxLength={42}
     />
   );
 };
