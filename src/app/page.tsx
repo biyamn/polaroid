979821de0c7b973ab36485373a028db90fc99121
biyamn <b1y9mn@gmail.com>
2024-10-26 17:06:17 +0900

    fix: 불필요한 prop 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,6 @@ export default function Home() {
           uploadImage={uploadImage}
           elementRef={elementRef}
           text={text}
-          uploadedImageUrl={uploadedImageUrl}
-          setUploadedImageUrl={setUploadedImageUrl}
           date={date}
         />
       ) : (
