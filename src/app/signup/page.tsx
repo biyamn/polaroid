1892b48f9d388352555aaf115bd2843485d270e8
biyamn <b1y9mn@gmail.com>
2025-01-12 12:36:00 +0900

    feat: 회원가입 supabase 코드 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,20 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { supabase } from '@/supabase/supabaseClient';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClickSignup = async () => {
+    console.log('click!');
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+  };
   return (
     <div className="flex flex-col items-center h-screen p-8">
       <div className="text-3xl py-10">Snap Diary</div>
@@ -9,6 +23,8 @@ export default function Signup() {
           <div className="text-[#FFA33C]">이메일</div>
           <input
             type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="pl-4 w-full h-12 border border-gray-300 rounded-md"
           />
         </div>
@@ -16,6 +32,8 @@ export default function Signup() {
           <div className="text-[#FFA33C]">비밀번호</div>
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="6글자 이상"
             className="pl-4 w-full h-12 border border-gray-300 rounded-md"
           />
@@ -37,7 +55,10 @@ export default function Signup() {
             </button>
           </Link>
         </div>
-        <button className="w-2/3 h-12 rounded-md text-lg my-4 text-white bg-[#FFA33C]">
+        <button
+          onClick={handleClickSignup}
+          className="w-2/3 h-12 rounded-md text-lg my-4 text-white bg-[#FFA33C]"
+        >
           가입하기
         </button>
       </div>
